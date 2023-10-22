9dbbfade8c9f61a6505f83e441bbfb10da10d697
Gabriela Drumond <gabi@normand.me>
2023-10-22 20:29:46 -0300

    //just rm curly braces

--- AULA7/aula/src/components/Item/Item.jsx
+++ AULA7/aula/src/components/Item/Item.jsx
@@ -1,17 +1,16 @@
 
-function Item ({item, onItemClick}){
-const handleOnItemClick = (item) => {
-    onItemClick(item);
-
-}
-{
-    return (
-        <li>
-            <p>{item.name}</p>
-            <button type="buttom" onClick={handleOnItemClick}>Ver Detalhes</button>
-        </li>
-    )
-  }
+function Item({ item, onItemClick }) {
+    const handleOnItemClick = (item) => {
+        onItemClick(item);
+     }
+    
+        return (
+            <li>
+                <p>{item.name}</p>
+                <button type="buttom" onClick={handleOnItemClick}>Ver Detalhes</button>
+            </li>
+        )
+    
 }
 
 export default Item;
